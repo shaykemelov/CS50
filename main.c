485871183aae8271ac33d154dd8cb38bed62527b
Shaykemelov <shaykemelov@gmail.com>
2017-10-16 03:07:52 -0700

    -Bugs

--- main.c
+++ main.c
@@ -7,17 +7,21 @@ void display_list(single_list*);
 int main(int argc, char *argv[]) 
 {
 	single_list *list = create(3);
+	if (list == NULL)
+	{
+		return 1;
+	}
 	list = insert(5, list);
 	list = insert(78, list);
 	
 	printf("List before deletion:\n");
 	display_list(list);
+	list = delete(4, &list);
+	//list = delete(5, &list);
+	//list = delete(3, &list);
+	list = delete(78, &list);
 	printf("List after deletion:\n");
-	delete(5, &list);
-	delete(3, &list);
-	delete(78, &list);
 	display_list(list);
-	
 	destroy(&list);	
 	display_list(list);
 	return 0;
--- single_list.c
+++ single_list.c
@@ -3,45 +3,60 @@
 single_list* create(int number)
 {
 	single_list *list = (single_list*)malloc(sizeof(single_list));
-	if (list != NULL)
+	if (list == NULL)
 	{
-		list->next = NULL;
-		list->number = number;
-		return list;
+		return NULL;
 	}
+
+	list->next = NULL;
+	list->number = number;
+	return list;
 }
 
 single_list* insert(int number, single_list *list)
 {
 	single_list *node = (single_list*)malloc(sizeof(single_list));
-	if (node != NULL)
+	if (node == NULL)
 	{
-		node->next = list;
-		node->number = number;
-		return node;
+		return list;
 	}
+
+	node->next = list;
+	node->number = number;
+	return node;
 }
 
 single_list* delete(int number, single_list **list)
 {
-	single_list *ptr = *list;
-	if (ptr->number == number)
+	single_list *prev = *list;
+	single_list *current = *list;
+	
+	int found = 0;
+	
+	while (current != NULL)
 	{
-		*list = ptr->next;
-		free(ptr);
-		ptr = NULL;
-		return *list;	
+		if (current->number == number)
+		{
+			found = 1;
+			break;
+		}
+		prev = current;
+		current = current->next;
 	}
 	
-	while ((ptr->next != NULL) && (ptr->next->number != number))
+	if (found)
 	{
-		ptr = ptr->next;
+		if (prev == current)
+		{
+			single_list **temp = &current->next;
+			free(current);
+			return *temp;
+		}
+		prev->next = current->next;
+		free(current);
+		current = NULL;
+		return prev;
 	}
-	
-	single_list *remove = ptr->next;
-	ptr->next = remove->next;
-	free(remove);
-	remove = NULL;
 	return *list;
 }
 
@@ -65,10 +80,12 @@ void destroy(single_list * *list)
 	{
 		return;
 	}
+	
 	if ((*list)->next != NULL)
 	{
 		destroy(&(*list)->next);
 	}
+	
 	free(*list);
 	*list = NULL;
 }
